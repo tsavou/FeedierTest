3c76d1a241ea0ee8110c9106835abcaaca992604
tsavou <theosavourat@gmail.com>
2024-08-05 00:01:35 +0200

    🎨 FeedbackCard

---
+++ resources/js/Components/FeedbackCard.vue
@@ -0,0 +1,92 @@
+<script setup>
+
+import DangerButton from '@/Components/DangerButton.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import { router } from '@inertiajs/vue3';
+
+
+defineProps({
+    feedback: Object,
+})
+
+const deleteFeedback = (id) => {
+    if (confirm('Are you sure you want to delete this feedback?')) {
+        router.delete(route('feedbacks.destroy', id), {
+            preserveScroll: true,
+        });
+    }
+}
+
+const restoreFeedback = (id) => {
+    router.post(route('feedbacks.restore', id), {
+        preserveScroll: true,
+    })
+}
+
+
+</script>
+
+
+
+<template>
+
+
+    <div class="bg-white shadow-sm sm:rounded-lg px-6 py-4 flex flex-col justify-between  ">
+
+        <div class="mb-6">
+
+            <div class="flex items-center justify-between">
+
+                <div v-if="feedback.source === 'DASHBOARD'">
+                    <div class="text-sm font-medium text-gray-900">
+                        {{ feedback.user_id ? feedback.user.name : '' }}
+                    </div>
+                    <div class="text-sm text-gray-500">
+                        {{ feedback.user_id ? feedback.user.email : feedback.email }}
+                    </div>
+                </div>
+
+                <div v-else>
+                    <div class="text-sm font-medium text-gray-900">
+                        Feedback from {{ feedback.source }}
+                    </div>
+
+                </div>
+
+                <div class="text-sm text-gray-500">
+                    {{ new Date(feedback.created_at).toLocaleDateString() }} at {{ new
+                        Date(feedback.created_at).toLocaleTimeString() }}
+                </div>
+            </div>
+
+            <div class="mt-4 ">
+                <p class="text-sm text-gray-500">
+                    {{ feedback.message }}
+                </p>
+            </div>
+        </div>
+
+
+        <div v-if="feedback.deleted_at" class="text-red-500 font-bold text-right">
+            <span>Deleted at {{ new Date(feedback.deleted_at).toLocaleDateString() }}</span>
+        </div>
+
+        <!-- Button -->
+        <div class="flex justify-end border-t border-gray-200 pt-2">
+            <DangerButton v-if="!feedback.deleted_at" @click="deleteFeedback(feedback.id)">
+                Delete
+            </DangerButton>
+
+
+            <PrimaryButton v-if="feedback.deleted_at" @click="restoreFeedback(feedback.id)">
+                Restore
+            </PrimaryButton>
+
+        </div>
+
+
+    </div>
+
+
+
+</template>
--- resources/js/Pages/Feedback/Index.vue
+++ resources/js/Pages/Feedback/Index.vue
@@ -1,26 +1,14 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
 import { router } from '@inertiajs/vue3';
+import DangerButton from '@/Components/DangerButton.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import FeedbackCard from '@/Components/FeedbackCard.vue';
 
 defineProps({
     feedbacks: Array,
 })
 
-
-
-const deleteFeedback = (id) => {
-    if (confirm('Are you sure you want to delete this feedback?')) {
-        router.delete(route('feedbacks.destroy', id), {
-            preserveScroll: true,
-        });
-    }
-}
-
-const restoreFeedback = (id) => {
-    router.post(route('feedbacks.restore', id), {
-        preserveScroll: true,
-    })
-}
 </script>
 
 
@@ -36,34 +24,16 @@ const restoreFeedback = (id) => {
         </template>
 
 
-        <div class="px-12">
+        <div class="py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div v-if="$page.props.flash.success"
-                class=" m-4 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
+                class="mb-4 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
                 role="alert"> {{
                     $page.props.flash.success }}</div>
 
-            <div v-for="feedback in feedbacks" :key="feedback.id"
-                class="m-4 bg-white border border-400 text-700 px-4 py-3 rounded relative">
 
-                <ul>
-                    <li v-if="feedback.user_id">User:{{ feedback.user.name }} </li>
-                    <li>Email: {{ feedback.user_id ? feedback.user.email : feedback.email }} </li>
-                    <li>MESSAGE{{ feedback.message }}</li>
-                    <li>CREATED AT: {{ new Date(feedback.created_at).toLocaleString() }}</li>
-                    <li>SOURCE: {{ feedback.source }}</li>
-                    <li v-if="feedback.deleted_at" class="text-red-500 font-bold">DELETED AT: {{ new
-                        Date(feedback.deleted_at).toLocaleString() }}</li>
-                </ul>
-
-                <button v-if="!feedback.deleted_at" @click="deleteFeedback(feedback.id)"
-                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
-
-                <button v-if="feedback.deleted_at" @click="restoreFeedback(feedback.id)"
-                    class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Restore</button>
-
+            <div class="grid lg:grid-cols-2 gap-4 ">
+                <FeedbackCard v-for="feedback in feedbacks" :feedback="feedback" />
             </div>
-
-
 
         </div>
 
